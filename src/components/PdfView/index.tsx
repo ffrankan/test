9fa0cd276cfde1c9780f2db132869b3dcbeffec6
yanhe <yanhe.liu96@gmail.com>
2025-02-22 22:09:06 +0800

    pref: pdf

--- src/components/PdfView/index.tsx
+++ src/components/PdfView/index.tsx
@@ -1,6 +1,6 @@
 import { FC, useRef, useEffect, useState } from 'react';
 import { styled } from 'styled-components';
-import { Document, Page, pdfjs } from'react-pdf';
+import { Document, Page, pdfjs, Outline } from'react-pdf';
 import { Button, message } from 'antd';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
@@ -11,14 +11,44 @@ const Container = styled.div`
   width: 100%;
   height: 100%;
   overflow: auto;
+  .pdf-toolbar {
+    position: fixed; // 固定定位
+    bottom: 3%; // 固定在底部
+    left: 50%;
+    width: fit-content;
+    height: 40px;
+    background-color: white; // 可以根据需要修改背景颜色
+    padding: 10px;
+    text-align: center;
+    transform: translateX(-50%);
+    Button {
+      margin: 0 10px;
+    }
+    z-index: 999;
+  }
+  
+  .pdf-container {
+    width: 100%;
+    height: 100%;
+  }
+  
+  .react-pdf__Page {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 `;
 
 const PdfViewer: FC = () => {
     const [pdfUrl, setPdfUrl] = useState<string>('public/test.pdf');
     const [numPages, setNumPages] = useState<number>(0);
+    const [height, setHeight] = useState<number>();
     const [pageNumber, setPageNumber] = useState<number>(1);
 
-    // 处理PDF加载成功
+    // 引用 canvas 元素和容器元素
+    const containerRef = useRef<HTMLDivElement | null>(null);
+
+    // 处理 PDF 加载成功
     const onLoadSuccess = ({ numPages }: any) => {
         setNumPages(numPages);
     };
@@ -26,28 +56,44 @@ const PdfViewer: FC = () => {
     // 处理页面点击事件
     const onPdfElementClick = (e: React.MouseEvent) => {
         const target = e.target as HTMLElement;
-        console.log(e, '----',target)
+        console.log(e, '----', target);
         // if (target && target.classList.contains('pdf-interactive')) {
         //     message.info('PDF元素被点击!');
         //     // 可以在这里处理交互逻辑，比如跳转到其他页面、显示更多内容等
         // }
     };
 
+    const changePage = (model: 'add' | 'minus') => {
+        model === 'add' ? setPageNumber(pageNumber + 1) : setPageNumber(pageNumber - 1);
+        //calculateScale()
+    }
+
+    // 在 canvas 加载完成后计算 scale
+    useEffect(() => {
+        setHeight(containerRef.current?.clientHeight)
+    }, [containerRef]);
+
     return (
-        <Container>
+        <Container ref={containerRef}>
             <div className="pdf-container" onClick={onPdfElementClick}>
                 <Document
                     file={pdfUrl}
                     onLoadSuccess={onLoadSuccess}
                 >
-                    <Page pageNumber={pageNumber} />
+                    <Page
+                        pageNumber={pageNumber}
+                        height={height || 300}
+                        // onRenderSuccess={(canvas) => {
+                        //     console.log(canvas)
+                        // }}
+                    />
                 </Document>
             </div>
             <div className="pdf-toolbar">
-                <Button onClick={() => setPageNumber(pageNumber - 1)} disabled={pageNumber <= 1}>
+                <Button onClick={() =>changePage('minus')} disabled={pageNumber <= 1}>
                     上一页
                 </Button>
-                <Button onClick={() => setPageNumber(pageNumber + 1)} disabled={pageNumber >= numPages}>
+                <Button onClick={() =>changePage('add')} disabled={pageNumber >= numPages}>
                     下一页
                 </Button>
             </div>
@@ -56,68 +102,3 @@ const PdfViewer: FC = () => {
 };
 
 export default PdfViewer;
-
-// const PdfView: FC = () => {
-//     const [numPages, setNumPages] = useState(null);
-//     const [scale, setScale] = useState(2);
-//     const canvasRefs = useRef<HTMLCanvasElement[]>([]);
-//     const pdfUrl = 'public/test.pdf';
-//
-//     const onPageLoadSuccess = (pdf): void => {
-//         setNumPages(pdf._pdfInfo.numPages);
-//
-//     };
-//
-//     const handleCanvasClick = (event, numPages) => {
-//         console.log('handleCanvasClick', event)
-//         console.log(canvasRefs, 'rrrrr')
-//
-//         if (canvasRefs.current) {
-//             const rect = canvasRefs.current.getBoundingClientRect();
-//             const x = event.clientX - rect.left;
-//             const y = event.clientY - rect.top;
-//             console.log(`Clicked on page ${numPages} at coordinates: (${x}, ${y})`);
-//         }
-//     };
-//
-//     const onPageRenderSuccess = (pageIndex, result) => {
-//         // 确保获取到的是原生的 HTMLCanvasElement
-//         const nativeCanvas = result.canvas as HTMLCanvasElement;
-//         //console.log('Page rendered', pageIndex, result)
-//         canvasRefs.current[pageIndex] = nativeCanvas;
-//         nativeCanvas && nativeCanvas.addEventListener('click', (event) => handleCanvasClick(event, pageIndex));
-//         return () => {
-//             nativeCanvas.removeEventListener('click', (event) => handleCanvasClick(event, pageIndex));
-//         };
-//     };
-//
-//     useEffect(() => {
-//         const canvas = canvasRefs.current;
-//         if (canvas) {
-//             canvas.addEventListener('click', handleCanvasClick);
-//             return () => {
-//                 canvas.removeEventListener('click', handleCanvasClick);
-//             };
-//         }
-//     }, [])
-//
-//     return (
-//         <Container ref={canvasRefs as any}>
-//             <Document
-//                 file={pdfUrl}
-//                 onLoadSuccess={onPageLoadSuccess}
-//             >
-//                 {numPages && Array.from(new Array(numPages), (_, index) => (
-//                     <Page
-//                         key={index + 'page'}
-//                         pageNumber={index + 1}
-//                         scale={scale}
-//                         //canvasRef={canvasRefs}
-//                         // onRenderSuccess={(result) => onPageRenderSuccess(index, result)}
-//                     />
-//                 ))}
-//             </Document>
-//         </Container>
-//     );
-// };
-//export default PdfView;
